f72168c114d306f779d84913d28f416ca1507fd2
Ian Knight <ian.knight.1990@gmail.com>
2025-05-19 18:48:36 +0100

    Refine Assembler interface

--- src/main/java/uk/ac/nott/cs/comp3012/tasm/Assembler.java
+++ src/main/java/uk/ac/nott/cs/comp3012/tasm/Assembler.java
@@ -12,22 +12,23 @@ public class Assembler {
 
   public static void main(String[] args) throws IOException {
     Assembler assembler = new Assembler();
-    assembler.assemble(args[0], args[1]);
+
+    CharStream chars = CharStreams.fromFileName(args[0]);
+    byte[] code = assembler.assemble(chars);
+
+    try (FileOutputStream out = new FileOutputStream(args[1])) {
+      out.write(code);
+    }
   }
 
-  public void assemble(String inFile, String outFile) throws IOException {
+  public byte[] assemble(CharStream input) {
     // construct lexer and parser
-    CharStream chars = CharStreams.fromFileName(inFile);
-    TasmLexer lexer = new TasmLexer(chars);
+    TasmLexer lexer = new TasmLexer(input);
     TokenStream tokens = new CommonTokenStream(lexer);
     TasmParser parser = new TasmParser(tokens);
 
     // read program
     InstructionList program = (InstructionList) new ProgramBuilder().visitProgram(parser.program());
-
-    // emit program
-    try (FileOutputStream out = new FileOutputStream(outFile)) {
-      out.write(program.toBytes());
-    }
+    return program.toBytes();
   }
 }
